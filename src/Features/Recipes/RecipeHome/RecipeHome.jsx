c51fa455d9260139e1a2407ce1107455b6e7a798
ellymaimon <eliranmaimon@gmail.com>
2018-09-28 18:52:41 -0700

    Add Recipe Home component

---
+++ src/Features/Recipes/RecipeHome/RecipeHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RecipeHome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Recipe Home</h1>
+      </div>
+    );
+  }
+}
+
+export default RecipeHome;
