3d181757ec0d91a36e06ca94140b787a283debbf
ellymaimon <eliranmaimon@gmail.com>
2018-09-28 18:32:10 -0700

    Configure index.js to allow for hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let render = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+if (module.hot) {
+  module.hot.accept("./App/App.jsx", () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
 registerServiceWorker();
