5c0322a41666d85f906c217cb2fb67f71fd59adb
ellymaimon <eliranmaimon@gmail.com>
2018-10-02 16:46:49 -0700

    Reformat navbar, add link to create recipe

--- src/Components/Nav/NavBar.jsx
+++ src/Components/Nav/NavBar.jsx
@@ -4,14 +4,14 @@ import { NavLink, Link, withRouter } from "react-router-dom";
 
 class NavBar extends Component {
   state = {
-    loggedIn: false
+    loggedIn: true
   };
 
   handleLogin = () => {
     this.setState({
       loggedIn: true
     });
-    this.props.history.push("/recipes")
+    this.props.history.push("/recipes");
   };
 
   handleLogout = () => {
@@ -25,15 +25,23 @@ class NavBar extends Component {
     const { loggedIn } = this.state;
     return (
       <Menu size="huge" fixed="top">
-        <Menu.Item as={Link} to="/recipes" name="Bread Baker's Apprentice" header />
+        <Menu.Item
+          as={Link}
+          to="/recipes"
+          name="Bread Baker's Apprentice"
+          header
+        />
         <Menu.Item as={NavLink} to="/example" name="Example" />
-        <Menu.Item position="right">
-          {loggedIn ? (
+        {loggedIn ? (
+          <Menu.Item position="right">
+            <Button content="Create Recipe" color="orange" inverted style={{ marginRight: "0.5em" }}/>
             <Button onClick={this.handleLogout} basic content="Logout" />
-          ) : (
+          </Menu.Item>
+        ) : (
+          <Menu.Item position="right">
             <Button onClick={this.handleLogin} basic content="Login" />
-          )}
-        </Menu.Item>
+          </Menu.Item>
+        )}
       </Menu>
     );
   }
