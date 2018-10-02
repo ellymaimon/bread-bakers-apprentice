8887762ac014875dbf0186d1c4c03549382b0c32
ellymaimon <eliranmaimon@gmail.com>
2018-10-02 16:47:24 -0700

    Add recipe detail content to component

--- src/Components/Recipes/RecipeDetail.jsx
+++ src/Components/Recipes/RecipeDetail.jsx
@@ -1,14 +1,46 @@
-import React, { Component } from 'react'
-import { Segment } from 'semantic-ui-react';
+import React from "react";
+import { connect } from "react-redux";
+import { Grid, Segment, Header, Image } from "semantic-ui-react";
+import Ingredient from "./Ingredient";
+import RecipeDetailHeader from "./RecipeDetailHeader";
+import RecipeInstructions from "./RecipeInstructions";
 
-export class RecipeDetail extends Component {
-  render() {
-    return (
-      <Segment>
-        
-      </Segment>
-    )
+const mapState = (state, ownProps) => {
+  const recipeId = ownProps.match.params.id;
+
+  let recipe = {};
+
+  if (recipeId && state.recipes.length > 0) {
+    recipe = state.recipes.filter(recipe => recipe.id === recipeId)[0];
   }
-}
 
-export default RecipeDetail
+  return {
+    recipe
+  };
+};
+
+const RecipeDetail = ({ recipe }) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+      <RecipeDetailHeader recipe={recipe} />
+      <RecipeInstructions recipe={recipe} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <Segment textAlign="center" attached="top" color="orange">
+          <Header>Ingredients</Header>
+        </Segment>
+        <Segment attached>
+          {recipe.ingredients.map(ingredient => (
+            <Ingredient ingredient={ingredient} />
+          ))}
+        </Segment>
+        <Segment>
+          <Image src={recipe.photoURL} size="large" />
+        </Segment>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default connect(mapState)(RecipeDetail);
