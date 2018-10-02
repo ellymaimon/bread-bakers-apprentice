36b2cdb31917a2fa82a379caae3f97d3a75d363f
ellymaimon <eliranmaimon@gmail.com>
2018-10-02 11:52:29 -0700

    Add two dummy data recipes

---
+++ src/Reducers/preLoadedData.jsx
@@ -0,0 +1,202 @@
+export const ingredients = {
+  flours: [],
+  salts: [],
+  yeasts: [],
+}
+
+export const dummyRecipes = [
+  {
+    id: "1",
+    title: "Tartine's Country Bread",
+    date: "2018-03-27",
+    photoURL: "/assets/tartine.jpg",
+    hydration: "75%",
+    description: "The country bread from Tartine Bakery in San Francisco has reached cult status among passionate bakers, and deservedly so.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Leaven",
+        quantity: 200,
+        measurement: "grams",
+        category: "leaven",
+      },
+      {
+        id: 2,
+        name: "Bread Flour",
+        quantity: 900,
+        measurement: "grams",
+        category: "flour",
+      },
+      {
+        id: 3,
+        name: "Whole Wheat Flour",
+        quantity: 100,
+        measurement: "grams",
+        category: "flour",
+      },
+      {
+        id: 4,
+        name: "Fine Sea Salt",
+        quantity: 20,
+        measurement: "grams",
+        category: "salt",
+      },
+      {
+        id: 5,
+        name: "Water",
+        quantity: 750,
+        measurement: "grams",
+        category: "water",
+        temperature: "80°"
+      }
+    ],
+    instructions: [
+      {
+        stepNumber: 1,
+        step: "Make the Leaven",
+        instruction: "Mix 1 tablespoon starter with 200g warm water. Add 200g of white/wheat flour mix and combine. Let rest 12 hours, or until tablespoon of leaven floats in room temp water."
+      },
+      {
+        stepNumber: 2,
+        step: "Add Leaven to Water",
+        instruction: "In a large bowl, combine 200 grams of leaven with 700 grams of warm water and stir to disperse."
+      },
+      {
+        stepNumber: 3,
+        step: "Add Flour",
+        instruction: "Add 900 grams of white-bread flour and 100 grams of whole-wheat flour to bowl and use your hands to mix until no traces of dry flour remain. The dough will be sticky and ragged. Cover bowl with a towel and let dough rest for 25 to 40 minutes at room temperature."
+      },
+      {
+        stepNumber: 4,
+        step: "Add Salt",
+        instruction: "Add 20 grams fine sea salt and 50 grams warm water. Use hands to integrate salt and water into dough thoroughly. The dough will begin to pull apart, but continue mixing; it will come back together."
+      },
+      {
+        stepNumber: 5,
+        step: "Let Dough Rise",
+        instruction: "Cover with a towel and let the dough rise for 30 minutes at about 75-80°F."
+      },
+      {
+        stepNumber: 6,
+        step: "Fold Dough",
+        instruction: "Fold dough by dipping hand in water, taking hold of the underside of the dough at one quadrant and stretching it up over the rest of the dough. Repeat this action 3 more times, rotating bowl a quarter turn for each fold. Do this every half-hour for 2 1/2 hours more (3 hours total). The dough should be billowy and increase in volume 20 to 30 percent. If not, continue to let rise and fold for up to an hour more."
+      },
+      {
+        stepNumber: 7,
+        step: "Cut, Round, and Rest",
+        instruction: "Transfer dough to a work surface and dust top with flour. Use a dough scraper to cut dough into 2 equal pieces and flip them over so floured sides are face down. Fold the cut side of each piece up onto itself so the flour on the surface remains entirely on the outside of the loaf; this will become the crust. Work dough into taut rounds. Place the dough rounds on a work surface, cover with a towel, and let rest 30 minutes."
+      },
+      {
+        stepNumber: 8,
+        step: "Line Baskets and Flour Towels",
+        instruction: "Line two 10- to 12-inch bread-proofing baskets or mixing bowls with towels. Use some rice/whole wheat flour mixture to generously flour towels (reserve some flour mixture for later)."
+      },
+      {
+        stepNumber: 9,
+        step: "Final Fold",
+        instruction: "Dust rounds with whole-wheat flour. Use a dough scraper to flip them over onto a work surface so floured sides are facing down. Take one round, and starting at the side closest to you, pull the bottom 2 corners of the dough down toward you, then fold them up into the middle third of the dough. Repeat this action on the right and left sides, pulling the edges out and folding them in over the center. Finally, lift the top corners up and fold down over previous folds. (Imagine folding a piece of paper in on itself from all 4 sides.) Roll dough over so the folded side becomes the bottom of the loaf. Shape into a smooth, taut ball. Repeat with other round."
+      },
+      {
+        stepNumber: 10,
+        step: "Final Rest",
+        instruction: "Transfer rounds, seam-side up, to prepared baskets. Cover with a towel and return dough to the 75- to 80-degree environment for 3 to 4 hours. (Or let dough rise for 10 to 12 hours in the refrigerator. Bring back to room temperature before baking.)"
+      },
+      {
+        stepNumber: 11,
+        step: "Score & Bake First Loaf",
+        instruction: "About 30 minutes before baking, place a Dutch oven or lidded cast-iron pot in the oven and heat it to 500 degrees. Dust tops of dough, still in their baskets, with whole-wheat/rice-flour mixture. Very carefully remove heated pot from oven and gently turn 1 loaf into pan seam-side down. Use a lame (a baker’s blade) or razor blade to score the top of the bread a few times to allow for expansion, cover and transfer to oven. Reduce temperature to 450 degrees and cook for 20 minutes. Carefully remove lid (steam may release) and cook for 20 more minutes or until crust is a rich, golden brown color."
+      },
+      {
+        stepNumber: 12,
+        step: "Score & Bake Second Loaf",
+        instruction: "Transfer bread to a wire rack to cool for at least 15 minutes before slicing. The bottom of the loaf should sound hollow when tapped. Increase oven temperature to 500 degrees, clean out pot and repeat this process with the second loaf."
+      },
+    ]
+  },
+  {
+    id: "2",
+    title: "King Arthur's French Baguette",
+    date: "2018-07-27",
+    photoURL: "/assets/baguette.jpg",
+    hydration: "75%",
+    description: "These crunchy baguettes feature a chewy interior riddled with holes, and a crisp, deep-gold crust.",
+    ingredients: [
+      {
+        id: 1,
+        name: "Water",
+        quantity: 113,
+        measurement: "grams",
+        category: "water",
+        temperature: "cool",
+        forPreferment: "starter"
+      },
+      {
+        id: 2,
+        name: "Instant Yeast",
+        quantity: 0.19,
+        measurement: "grams",
+        category: "yeast",
+        forPreferment: "starter"
+      },
+      {
+        id: 3,
+        name: "All-Purpose Flour",
+        quantity: 120,
+        measurement: "grams",
+        category: "flour",
+        forPreferment: "starter"
+      },
+      {
+        id: 4,
+        name: "Instant Yeast",
+        quantity: 4.65,
+        measurement: "grams",
+        category: "yeast",
+      },
+      {
+        id: 5,
+        name: "Water",
+        quantity: 255,
+        measurement: "grams",
+        category: "water",
+        temperature: "lukewarm",
+      },
+      {
+        id: 6,
+        name: "All-Purpose Flour",
+        quantity: 418,
+        measurement: "grams",
+        category: "flour",
+      },
+      {
+        id: 6,
+        name: "Salt",
+        quantity: 11.38,
+        measurement: "grams",
+        category: "salt",
+      },
+    ],
+    instructions: [
+      {
+        stepNumber: 1,
+        step: "Make the Starter",
+        instruction: "To make the starter: Mix all starter ingredients together to make a soft dough. Cover and let rest at room temperature for about 14 hours; overnight works well. The starter should have expanded and become bubbly."
+      },
+      {
+        stepNumber: 2,
+        step: "Make Dough",
+        instruction: "Mix and knead all dough ingredients together to make a soft, somewhat smooth dough; it should be cohesive, but the surface may still be a bit rough. If you're using a stand mixer, knead for about 4 minutes on medium-low speed (speed 2 on a KitchenAid); the finished dough should stick a bit at the bottom of the bowl."
+      },
+      {
+        stepNumber: 3,
+        step: "Rise and Deflate",
+        instruction: "Place the dough in a lightly greased medium-sized bowl, cover the bowl, and let the dough rise for 90 minutes, gently deflating it, folding the edges into the center, and turning it over after 45 minutes."
+      },
+      {
+        stepNumber: 4,
+        step: "Divide into Three",
+        instruction: "Turn the dough out onto a lightly greased work surface. Gently deflate it, and divide it into three equal pieces."
+      },
+    ]
+  }
+];
