102a466bcf73bd378512fcae9afb145d639a93ce
ellymaimon <eliranmaimon@gmail.com>
2018-10-02 11:52:13 -0700

    Connect recipe main component to redux store and pass down recipes

--- src/Components/Recipes/RecipeMain.jsx
+++ src/Components/Recipes/RecipeMain.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import RecipeFeed from "./RecipeFeed";
 import { Grid } from 'semantic-ui-react'
 
+const mapState = state => ({
+  recipes: state.recipes
+});
+
 class RecipeMain extends Component {
   render() {
+    const { recipes } = this.props;
     return (
       <Grid>
-        <RecipeFeed />
+        <RecipeFeed recipes={recipes} />
       </Grid>
     );
   }
 }
 
-export default RecipeMain;
+export default connect(mapState)(RecipeMain);
