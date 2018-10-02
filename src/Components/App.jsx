83174f9ce04286122c7529a7e75689f5cecf305d
ellymaimon <eliranmaimon@gmail.com>
2018-10-01 22:01:24 -0700

    Add routes in App to homepage and to the example test page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RecipeHome from "./Recipes/RecipeHome";
 import { NavBar } from "./Nav/NavBar";
 import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
 import ExampleComponent from "./ExampleComponent";
 
 class App extends Component {
@@ -10,8 +11,10 @@ class App extends Component {
       <div>
         <NavBar />
         <Container className="main">
-          <RecipeHome />
-          <ExampleComponent />
+          <Switch>
+            <Route exact path="/" component={RecipeHome} />
+            <Route path="/example" component={ExampleComponent} />
+          </Switch>
         </Container>
       </div>
     );
