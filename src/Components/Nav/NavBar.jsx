54d2af0a36b45cc53217c85d8e71a498823f21d5
ellymaimon <eliranmaimon@gmail.com>
2018-10-01 22:04:11 -0700

    Fix links in navbar

--- src/Components/Nav/NavBar.jsx
+++ src/Components/Nav/NavBar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { Menu, Button } from "semantic-ui-react";
-import { NavLink, withRouter } from "react-router-dom";
+import { NavLink, Link, withRouter } from "react-router-dom";
 
 export class NavBar extends Component {
   render() {
     return (
       <Menu size="huge" inverted fixed="top">
-        <Menu.Item name="Logo" header />
-        <Menu.Item name="Recipes" />
+        <Menu.Item as={Link} to="/" name="Home" header />
+        <Menu.Item as={NavLink} to="/myrecipes" name="My Recipes" />
         <Menu.Item as={NavLink} to="/example" name="Example" />
         <Menu.Item position="right">
           <Button basic inverted content="Login" />
