b79b4a5be956dea162158cbe435e79c3b1b27913
ellymaimon <eliranmaimon@gmail.com>
2018-10-02 16:47:12 -0700

    Reformat recipe component

--- src/Components/Recipes/Recipe.jsx
+++ src/Components/Recipes/Recipe.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from "react";
-import { Item, Icon, Popup } from "semantic-ui-react";
-import Ingredient from "./Ingredient";
+import { Item, Icon, Popup, Segment } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export class Recipe extends Component {
   render() {
     const { recipe } = this.props;
     return (
-      <Item.Group>
-        <Item>
-          <Item.Image size="small" src={recipe.photoURL} />
-          <Item.Content>
-            <Item.Header>{recipe.title}</Item.Header>
-            <Item.Meta>
-              <Popup
-                trigger={<Icon name="tint" />}
-                content="Hydration Percentage: The amount of water relative to the amount of flour in the dough."
-              />{" "}
-              {recipe.hydration}
-            </Item.Meta>
-            <Item.Description>{recipe.description}</Item.Description>
-            <Item.Meta>Ingredients:</Item.Meta>
-            <Item.Description>
-              {recipe.ingredients.map(ingredient => (
-                <Ingredient ingredient={ingredient} key={ingredient.id} />
-              ))}
-            </Item.Description>
-            <Item.Extra>Created: {recipe.date}</Item.Extra>
-          </Item.Content>
-        </Item>
-      </Item.Group>
+      <Segment raised>
+        <Item.Group>
+          <Item>
+            <Item.Image size="small" src={recipe.photoURL} />
+            <Item.Content>
+              <Item.Header as={Link} to={`/recipe/${recipe.id}`}>
+                {recipe.title}
+              </Item.Header>
+              <Item.Meta>
+                <Popup
+                  trigger={<Icon name="tint" />}
+                  content="Hydration Percentage: The amount of water relative to the amount of flour in the dough."
+                />{" "}
+                {recipe.hydration}
+              </Item.Meta>
+              <Item.Description>{recipe.description}</Item.Description>
+              <Item.Extra>
+                Created by: {recipe.createdBy} on {recipe.date}
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
     );
   }
 }
