156734e9d726661219c5765decd754a20e371136
ellymaimon <eliranmaimon@gmail.com>
2018-10-01 21:53:08 -0700

    Add provider and store configuration to index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
-import App from "./App";
+import App from "./Components/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { storeConfig } from "./Store/storeConfig";
+
+const store = storeConfig();
 
 let render = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, document.getElementById("root"));
 };
 
 if (module.hot) {
-  module.hot.accept("./App.jsx", () => {
+  module.hot.accept("./Components/App.jsx", () => {
     setTimeout(render);
   });
 }
