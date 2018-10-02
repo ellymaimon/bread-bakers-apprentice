2407b0a2c58824dfc42e3eeb36b3c73cff598dfd
ellymaimon <eliranmaimon@gmail.com>
2018-10-02 11:54:10 -0700

    Add recipe detail component

---
+++ src/Components/Recipes/RecipeDetail.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Segment } from 'semantic-ui-react';
+
+export class RecipeDetail extends Component {
+  render() {
+    return (
+      <Segment>
+        
+      </Segment>
+    )
+  }
+}
+
+export default RecipeDetail
