953ad85a73873824ed690077e863a399aca2db6d
ellymaimon <eliranmaimon@gmail.com>
2018-10-03 10:28:02 -0700

    Add form reducer to root reducer

--- src/Reducers/recipeReducer.jsx
+++ src/Reducers/recipeReducer.jsx
@@ -5,7 +5,7 @@ import { CREATE_RECIPE } from '../Constants/recipeConstants'
 const initialState = dummyRecipes;
 
 export const createRecipe = (state, payload) => {
-  return [...state, Object.assign({}, payload.event)]
+  return [...state, Object.assign({}, payload.recipe)]
 }
 
 export default createReducer(initialState, {
--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form'
 import exampleReducer from './exampleReducer';
 import recipeReducer from './recipeReducer'
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   example: exampleReducer,
   recipes: recipeReducer
 })
