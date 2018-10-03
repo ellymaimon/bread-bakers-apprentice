81614b5f4a41e4331e3952f94bb25c579b9429e2
ellymaimon <eliranmaimon@gmail.com>
2018-10-03 10:27:45 -0700

    Add field array for ingredients in create recipe form

--- src/Components/Forms/RecipeForm.jsx
+++ src/Components/Forms/RecipeForm.jsx
@@ -9,10 +9,10 @@ import {
   Grid,
   List
 } from "semantic-ui-react";
+import cuid from "cuid";
 import { combineValidators, isRequired } from "revalidate";
-import { Field, reduxForm } from "redux-form";
+import { Field, FieldArray, reduxForm } from "redux-form";
 import { createRecipe } from "../../Actions/recipeActions";
-import Ingredient from "../Recipes/Ingredient";
 import TextInput from "./TextInput";
 import TextAreaInput from "./TextAreaInput";
 
@@ -24,7 +24,7 @@ const mapState = (state, ownProps) => {
   if (recipeId && state.recipes.length > 0) {
     recipe = state.recipes.filter(recipe => recipe.id === recipeId)[0];
   }
-  
+
   return {
     initialValues: recipe
   };
@@ -39,14 +39,73 @@ const formValidation = combineValidators({
   description: isRequired({ message: "The recipe description is required" })
 });
 
+const renderIngredients = ({ fields, meta: { error, submitFailed } }) => (
+  <div>
+    <Button
+      color="orange"
+      type="button"
+      onClick={() => fields.push({})}
+      style={{ marginBottom: "0.8em" }}
+    >
+      <Icon name="add circle" /> Add Ingredient
+    </Button>
+    {submitFailed && error && <span>{error}</span>}
+    {fields.map((ingredients, index) => (
+      <Segment key={index}>
+        <h4>Ingredient #{index + 1}</h4>
+        <Field
+          name={`${ingredients}.name`}
+          type="text"
+          component={TextInput}
+          placeholder="Ingredient name"
+        />
+        <Field
+          name={`${ingredients}.grams`}
+          type="text"
+          component={TextInput}
+          placeholder="Weight in grams"
+        />
+        <Button
+          type="button"
+          content="Remove Ingredient"
+          onClick={() => fields.remove(index)}
+          style={{ marginBottom: "0.8em" }}
+        >
+          <Icon name="remove circle" /> Remove Ingredient{" "}
+        </Button>
+      </Segment>
+    ))}
+  </div>
+);
+
 class RecipeForm extends Component {
+  onFormSubmit = values => {
+    if (this.props.initialValues.id) {
+      this.props.updateRecipe(values);
+      this.props.history.goBack();
+    } else {
+      const newRecipe = {
+        ...values,
+        id: cuid(),
+        photoURL: "/assets/user.png",
+        createdBy: "Epicodus Student"
+      };
+      this.props.createRecipe(newRecipe);
+      this.props.history.push("/recipes");
+    }
+  };
+
   render() {
+    const { invalid, submitting, pristine } = this.props;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <Segment>
+          <Segment style={{ paddingBottom: "1em" }}>
             <Header as="h2" content="Create a Recipe" />
-            <Form>
+            <Form
+              onSubmit={this.props.handleSubmit(this.onFormSubmit)}
+              id="recipeForm"
+            >
               <Field
                 name="title"
                 type="text"
@@ -60,10 +119,12 @@ class RecipeForm extends Component {
                 component={TextAreaInput}
                 placeholder="Description"
               />
-              <Button color="orange" type="button">
-                <Icon name="add circle" /> Add Ingredient
-              </Button>
-              <Button floated="right" positive type="submit">
+              <Button
+                floated="right"
+                positive
+                type="submit"
+                disabled={invalid || submitting || pristine}
+              >
                 Submit
               </Button>
               <Button
@@ -73,6 +134,7 @@ class RecipeForm extends Component {
               >
                 Cancel
               </Button>
+              <FieldArray name="ingredients" component={renderIngredients} />
             </Form>
           </Segment>
         </Grid.Column>
