57477c44d3fe5756f622a0fcff4a3e33cb236801
ellymaimon <eliranmaimon@gmail.com>
2018-10-04 08:04:46 -0700

    Remove unnecessary reducer from root

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as FormReducer } from 'redux-form'
-import exampleReducer from './exampleReducer';
 import recipeReducer from './recipeReducer'
 
 const rootReducer = combineReducers({
   form: FormReducer,
-  example: exampleReducer,
   recipes: recipeReducer
 })
 
