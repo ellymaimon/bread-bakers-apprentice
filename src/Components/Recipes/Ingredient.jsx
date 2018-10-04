6d2e26f9eb1e4e309122f8a0e23f21aaaecf6ee3
ellymaimon <eliranmaimon@gmail.com>
2018-10-04 09:17:31 -0700

    Add functionality to convert between metric and imperial measurements in ingredient component

--- src/Components/Recipes/Ingredient.jsx
+++ src/Components/Recipes/Ingredient.jsx
@@ -6,14 +6,14 @@ const listContentStyle = {
   gridTemplateColumns: "50% 30% 20%",
   justifyItems: "start",
   alignItems: "center"
-}
+};
 
 const ingredientNameSpanStyle = {
   fontSize: "1.1em",
   fontWeight: "bold",
   margin: "0 1em 0.2em 0",
   lineHeight: "1.5em"
-}
+};
 
 const ingredientNumbersSpanStyle = {
   justifySelf: "center",
@@ -21,11 +21,35 @@ const ingredientNumbersSpanStyle = {
   borderRadius: "2px",
   padding: "7px",
   color: "white"
-}
+};
+
+
 
 class Ingredient extends Component {
+  state = {
+    isMetric: true
+  };
+
+  handleConvertToImperial = () => {
+    this.setState({
+      isMetric: false
+    })
+  };
+
+  handleConvertToMetric = () => {
+    this.setState({
+      isMetric: true
+    })
+  }
+
+  getOunces =(grams) => {
+    grams = grams * 0.035274;
+    return parseFloat(grams).toFixed(2);
+  }
+
   render() {
     const { ingredient } = this.props;
+    const { isMetric } = this.state;
     return (
       <List.Item>
         <List.Content style={listContentStyle}>
@@ -34,8 +58,17 @@ class Ingredient extends Component {
             {ingredient.forPreferment && `For ${ingredient.forPreferment}: `}
             {ingredient.name}
           </span>
-          <span style={ingredientNumbersSpanStyle}>{ingredient.grams}g</span>
-          <span style={ingredientNumbersSpanStyle}>{ingredient.bakersPercentage ? ingredient.bakersPercentage : "n/a"}</span>
+          {isMetric ? (
+            <span onClick={this.handleConvertToImperial} style={ingredientNumbersSpanStyle}>{ingredient.grams}g</span>
+          ) : (
+            <span onClick={this.handleConvertToMetric} style={ingredientNumbersSpanStyle}>
+              {this.getOunces(ingredient.grams)}
+              oz
+            </span>
+          )}
+          <span style={ingredientNumbersSpanStyle}>
+            {ingredient.bakersPercentage ? ingredient.bakersPercentage : "n/a"}
+          </span>
         </List.Content>
       </List.Item>
     );
