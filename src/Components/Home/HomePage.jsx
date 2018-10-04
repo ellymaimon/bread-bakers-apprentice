0205ff732b1e9605bb137a553ccc46fc9f8e2070
ellymaimon <eliranmaimon@gmail.com>
2018-10-03 18:58:10 -0700

    Add homepage content and styling

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { Button, Icon } from "semantic-ui-react";
 
-const HomePage = () => {
+const background = "../assets/bg.jpg";
+
+const imgBg = {
+  minHeight: "100%",
+  minWidth: "1024px",
+  width: "100%",
+  height: "auto",
+  position: "fixed",
+  top: "0",
+  left: "0",
+  filter: "brightness(30%)"
+};
+
+const buttonStyle = {
+  position: "absolute",
+  top: "80%",
+  right: "10%"
+};
+
+const HomePage = ({ history }) => {
   return (
     <div>
-      <h1>Welcome to Bread Baker's Apprentice</h1>
+      <img src={background} style={imgBg} alt="Background bread" />
+      <h1 className="homepage">Bread Baker's Apprentice</h1>
+      <Button
+        color="orange"
+        size="massive"
+        inverted
+        style={buttonStyle}
+        onClick={() => history.push("/recipes")}
+      >
+        {" "}
+        Enter <Icon name="right arrow icon" />{" "}
+      </Button>
     </div>
   );
 };
