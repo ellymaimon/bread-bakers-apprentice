b189e60c93a5fe40a04d5ed50c35ac1d284b96d1
ellymaimon <eliranmaimon@gmail.com>
2018-10-03 19:51:03 -0700

    Add delete recipe action and function call to recipe main component

--- src/Components/Recipes/RecipeMain.jsx
+++ src/Components/Recipes/RecipeMain.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import RecipeFeed from "./RecipeFeed";
-import { Grid } from 'semantic-ui-react'
+import { Grid } from "semantic-ui-react";
+import { deleteRecipe } from "../../Actions/recipeActions";
 
 const mapState = state => ({
   recipes: state.recipes
 });
 
+const actions = {
+  deleteRecipe
+};
+
 class RecipeMain extends Component {
+  handleDeleteRecipe = recipeId => () => {
+    this.props.deleteRecipe(recipeId);
+  };
+
   render() {
     const { recipes } = this.props;
     return (
       <Grid>
-        <RecipeFeed recipes={recipes} />
+        <RecipeFeed recipes={recipes} deleteRecipe={this.handleDeleteRecipe} />
       </Grid>
     );
   }
 }
 
-export default connect(mapState)(RecipeMain);
+export default connect(
+  mapState,
+  actions
+)(RecipeMain);
