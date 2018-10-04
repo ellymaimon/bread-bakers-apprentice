4f385f702c7a9440289a289080911bd2705cd752
ellymaimon <eliranmaimon@gmail.com>
2018-10-03 19:47:44 -0700

    Add update and delete recipe actions

--- src/Actions/recipeActions.jsx
+++ src/Actions/recipeActions.jsx
@@ -1,10 +1,28 @@
-import { CREATE_RECIPE } from '../Constants/recipeConstants';
+import { CREATE_RECIPE, UPDATE_RECIPE, DELETE_RECIPE } from '../Constants/recipeConstants';
 
 export const createRecipe = (recipe) => {
   return {
     type: CREATE_RECIPE,
     payload: {
       recipe
+    }
+  }
+}
+
+export const updateRecipe = (recipe) => {
+  return {
+    type: UPDATE_RECIPE,
+    payload: {
+      recipe
+    }
+  }
+}
+
+export const deleteRecipe = (recipeId) => {
+  return {
+    type: DELETE_RECIPE,
+    payload: {
+      recipeId
     }
   }
 }
