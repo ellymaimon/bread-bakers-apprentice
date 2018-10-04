89c238ade0ab517433e9871660c99482fd55ed5c
ellymaimon <eliranmaimon@gmail.com>
2018-10-03 19:51:34 -0700

    Add update and delete recipe reducer functions

--- src/Reducers/recipeReducer.jsx
+++ src/Reducers/recipeReducer.jsx
@@ -1,6 +1,6 @@
 import { createReducer } from '../Reducers/reducerUtility';
 import { dummyRecipes } from './preLoadedData';
-import { CREATE_RECIPE } from '../Constants/recipeConstants'
+import { CREATE_RECIPE, UPDATE_RECIPE, DELETE_RECIPE } from '../Constants/recipeConstants'
 
 const initialState = dummyRecipes;
 
@@ -8,6 +8,19 @@ export const createRecipe = (state, payload) => {
   return [...state, Object.assign({}, payload.recipe)]
 }
 
+export const updateRecipe = (state, payload) => {
+  return [
+    ...state.filter(recipe => recipe.id !== payload.recipe.id),
+    Object.assign({}, payload.recipe)
+  ]
+}
+
+export const deleteRecipe = (state, payload) => {
+  return [...state.filter(recipe => recipe.id !== payload.recipeId)]
+}
+
 export default createReducer(initialState, {
-  [CREATE_RECIPE]: createRecipe
+  [CREATE_RECIPE]: createRecipe,
+  [UPDATE_RECIPE]: updateRecipe,
+  [DELETE_RECIPE]: deleteRecipe
 });
